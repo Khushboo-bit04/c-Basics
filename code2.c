33178ac642a150867bd31b8cf20f83cbf30744ad
Khushboo-bit04 <ky131103@gmail.com>
2025-09-20 22:04:23 +0530

    initial repo

---
+++ code2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int age = 20;
+    float pi = 3.14;
+    char grade = 'A';
+    printf("Age: %d\n" , age);
+    printf("pi: %.2f\n" , pi);
+    printf("grade: %c\n" , grade);
+    return 0;
+}
