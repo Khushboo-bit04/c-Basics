a8c1bbf42f94f2473f27b07f0bdb8e5226b98789
Khushboo-bit04 <ky131103@gmail.com>
2025-11-03 16:42:05 +0000

    Nov 3, 2025, 10:12 PM

---
+++ code5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+printf("Name: Khushboo Yadav\n");
+printf("Course: B.tech\n");
+printf("Branch: Computer Science\n");
+printf("\tKeep learning and keep going!\n");
+printf("\a");
+return 0;
+}
